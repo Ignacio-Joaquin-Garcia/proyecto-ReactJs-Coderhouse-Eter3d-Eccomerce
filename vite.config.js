9640fbffe6fc7494c2bb9f334e4dbc99507f3101
ignaciogarcia <garciaignaciouni@gmail.com>
2025-07-28 04:16:06 -0300

    solucion github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/proyecto-ReactJs-Coderhouse-Eter3d-Eccomerce/', // 👈 reemplazá esto con el nombre real de tu repo
   plugins: [react()],
 })
