f08eb9df6ce0f289d6d5c7f67d25c8a7c6d027cc
ignaciogarcia <garciaignaciouni@gmail.com>
2025-09-17 22:17:07 -0300

    ProyectoFinal+Garcia

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/proyecto-ReactJs-Coderhouse-Eter3d-Eccomerce/', 
+  base: '/', 
   plugins: [react()],
 })
+//proyecto-ReactJs-Coderhouse-Eter3d-Eccomerce/
